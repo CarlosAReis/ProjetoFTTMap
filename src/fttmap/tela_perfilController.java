692d3534a450993edfdc42dd2a100eae2e74a477
lhamavoadora1 <53661607+lhamavoadora1@users.noreply.github.com>
2019-09-22 16:12:06 -0300

    adding controllers

---
+++ src/fttmap/tela_perfilController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fttmap;
+
+/**
+ *
+ * @author Caue
+ */
+public class tela_perfilController {
+    
+}
