c413dfa485859ed6c374f5cf00abd386b35b8955
Jonatas <jojokl@hotmail.com>
2019-09-22 11:35:56 -0300

    Adiciona tela de login

--- src/fttmap/FTTMap.java
+++ src/fttmap/FTTMap.java
@@ -19,7 +19,7 @@ public class FTTMap extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("tela_login.fxml"));
         
         Scene scene = new Scene(root);
         
